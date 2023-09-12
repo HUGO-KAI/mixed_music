df79113ac2dbb040ae904a62bdd371ba36921662
hugo <devhugok@gmail.com>
2023-09-12 17:00:35 +0200

    lesson 15

--- src/Controller/SongApiController.php
+++ src/Controller/SongApiController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 //use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,13 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class SongApiController extends AbstractController
 {
     #[Route('/api/songs/{id<\d+>}', methods:['GET'])]
-    public function getSong (int $id):Response
+    public function getSong (int $id, LoggerInterface $logger):Response
     {
         $song = [
             'id' => $id,
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+        $logger->info('Returning API response for song '.$id);
         return $this->json($song);
     }
 }
